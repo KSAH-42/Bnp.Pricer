0ac10e284bdb852ff51eceae96ebb53852689258
KSAH-42 <63054588+KSAH-42@users.noreply.github.com>
2020-04-03 09:55:42 +0200

    Update UIEnvironment.cs

--- Bnp.Pricer.Module/Windows/UIEnvironment.cs
+++ Bnp.Pricer.Module/Windows/UIEnvironment.cs
@@ -5,7 +5,7 @@ namespace Bnp.Pricer.Windows
 	using Bnp.Pricer.Configuration;
 
 	/// <summary>
-	/// Represent the common adapter between ui objects and configuration settings. This is class is for reducing coupling between ui objects and others class
+	/// Represent the bridge between ui objects that need consume parameter and configuration settings. This is class is for reducing coupling between ui objects and for example the configuration classes
 	/// </summary>
 	public static class UIEnvironment
 	{
