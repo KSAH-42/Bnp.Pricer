e6d6a16131b33b7a5f2a2e956d75c65db270a4d0
KSAH-42 <63054588+KSAH-42@users.noreply.github.com>
2020-04-03 13:17:49 +0200

    Update TypeExtensionHelpers.cs
    Rename somes parameters

--- Bnp.Pricer.Module/Data/TypeExtensionHelpers.cs
+++ Bnp.Pricer.Module/Data/TypeExtensionHelpers.cs
@@ -10,28 +10,28 @@ namespace Bnp.Pricer.Data
 		/// <summary>
 		/// Round the value
 		/// </summary>
-		/// <param name="valueType">the value</param>
+		/// <param name="value">the value</param>
 		/// <returns>Returns a value.</returns>
-		public static decimal ToRound( this decimal valueType )
+		public static decimal ToRound( this decimal value )
 		{
-			return Math.Round( valueType );
+			return Math.Round( value );
 		}
 
 		/// <summary>
 		/// Round the value
 		/// </summary>
-		/// <param name="valueType">the value</param>
+		/// <param name="value">the value</param>
 		/// <param name="numberOfDigits">the number of digits</param>
 		/// <returns>Returns a value.</returns>
 		/// <exception cref="ArgumentException"/>
-		public static decimal ToRound( this decimal valueType , int numberOfDigits )
+		public static decimal ToRound( this decimal value , int numberOfDigits )
 		{
 			if ( 0 >= numberOfDigits )
 			{
 				throw new ArgumentException( nameof( numberOfDigits ) );
 			}
 
-			return Math.Round( valueType , numberOfDigits );
+			return Math.Round( value , numberOfDigits );
 		}
 	}
 }
